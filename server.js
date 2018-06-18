308bc663f90916bc86b271665d51d55e909b960b
Zachary Guns <zacharyguns@yahoo.com>
2018-06-18 09:26:42 -0700

    almsot there but still failed

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// need help here, we're not appending the survey info,
+// we want to get an answer back with the user input
+function addinfo() {
+  $(".input-field").prompt(friendList);
+}
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
