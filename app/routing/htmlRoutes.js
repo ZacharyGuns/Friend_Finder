db106bed4547cf03760d7b34fd6aad29d33ad485
Zachary Guns <zacharyguns@yahoo.com>
2018-06-17 22:47:48 -0700

    i dont know what im doing

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,17 +1,25 @@
-// Pull in required dependencies
-var path = require('path');
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
-// Export HTML routes
-module.exports = function(app) {
-	// console.log('___ENTER htmlRoutes.js___');
 
-	// Home page
-	app.get('/', function(req, res) {
-		res.sendFile(path.join(__dirname, '../public/home.html'));
-	});
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/home.html'));
+});
 
-	// Survey page
-	app.get('/survey', function(req, res) {
-		res.sendFile(path.join(__dirname, '../public/survey.html'));
-	});
-};
+app.get('/survey', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/survey.html'));
+});
+
+
+app.post("/api/survey", function (req, res) {
+    var newUser = req.body;
+
+    newUser.routeName = newUser.name.replace(/\s+/g, "").toLowerCase();
+
+    console.log(newUser);
+
+    input-field.push(newUser);
+
+    res.json(newUser);
+});
--- server.js
+++ server.js
@@ -7,3 +7,9 @@ var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  });
+  
